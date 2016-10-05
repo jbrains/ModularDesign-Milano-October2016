c134588c5cb952535f0cf038aa860d8edb95ae9a
J. B. Rainsberger <me@jbrains.ca>
2016-10-05 09:37:00 +0200

    The Controller now gracefully handles not finding a price.

--- src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -3,7 +3,6 @@ package ca.jbrains.pos.test;
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -19,11 +18,25 @@ public class SellOneItemControllerTest {
 
     @Test
     public void productFound() throws Exception {
+        final Price foundPrice = Price.cents(580);
+        
         context.checking(new Expectations() {{
             allowing(catalog).findPrice(with("::irrelevant barcode::"));
-            will(returnValue(Price.cents(580)));
+            will(returnValue(foundPrice));
 
-            oneOf(display).displayPrice(with(Price.cents(580)));
+            oneOf(display).displayPrice(with(foundPrice));
+        }});
+
+        new SellOneItemController(catalog, display).onBarcode("::irrelevant barcode::");
+    }
+
+    @Test
+    public void productNotFound() throws Exception {
+        context.checking(new Expectations() {{
+            allowing(catalog).findPrice(with("::irrelevant barcode::"));
+            will(returnValue(null));
+
+            oneOf(display).displayProductNotFoundMessage(with("::irrelevant barcode::"));
         }});
 
         new SellOneItemController(catalog, display).onBarcode("::irrelevant barcode::");
@@ -32,8 +45,11 @@ public class SellOneItemControllerTest {
     public interface Catalog {
         Price findPrice(String barcode);
     }
+
     public interface Display {
         void displayPrice(Price price);
+
+        void displayProductNotFoundMessage(String barcodeNotFound);
     }
 
     private static class SellOneItemController {
@@ -46,13 +62,17 @@ public class SellOneItemControllerTest {
         }
 
         public void onBarcode(String barcode) {
-            display.displayPrice(catalog.findPrice(barcode));
+            final Price price = catalog.findPrice(barcode);
+            if (price == null)
+                display.displayProductNotFoundMessage(barcode);
+            else
+                display.displayPrice(price);
         }
     }
 
     public static class Price {
         public static Price cents(int priceInCents) {
-            return null;
+            return new Price();
         }
     }
 }
