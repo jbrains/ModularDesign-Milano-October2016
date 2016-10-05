44fcf1a157ce79f1bedaa567dda9ae454bb89383
J. B. Rainsberger <me@jbrains.ca>
2016-10-05 09:51:19 +0200

    The production catalog now respects the contract on Catalog so far.

--- src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
@@ -3,6 +3,7 @@ package ca.jbrains.pos.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +13,21 @@ public class InMemoryCatalogTest {
         final Price foundPrice = Price.cents(795);
 
         Assert.assertEquals(foundPrice, catalogWith("12345", foundPrice).findPrice("12345"));
+    }
+
+    @Test
+    public void productNotFound() throws Exception {
+        final InMemoryCatalog catalog = catalogWithout("::unknown product::");
+
+        Assert.assertEquals(null, catalog.findPrice("::unknown product::"));
+    }
+
+    private InMemoryCatalog catalogWithout(String barcodeToAvoid) {
+        return new InMemoryCatalog(new HashMap<String, Price>() {{
+            put(String.format("Not %s", barcodeToAvoid), Price.cents(1));
+            put(String.format("Absolutely not %s", barcodeToAvoid), Price.cents(2));
+            put(String.format("Still, completely and totally not %s", barcodeToAvoid), Price.cents(3));
+        }});
     }
 
     private InMemoryCatalog catalogWith(final String barcode, final Price foundPrice) {
--- src/test/java/ca/jbrains/pos/test/Price.java
+++ src/test/java/ca/jbrains/pos/test/Price.java
@@ -4,4 +4,7 @@ public class Price {
     public static Price cents(int priceInCents) {
         return new Price();
     }
+    public String toString() {
+        return "a Price";
+    }
 }
